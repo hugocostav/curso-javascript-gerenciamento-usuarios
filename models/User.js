5bce6373d879ff0abd5242b3007b8e2301a670d6
hugocostav <hugocostam100@outlook.com.br>
2021-08-28 23:50:19 -0300

    User constructor

---
+++ models/User.js
@@ -0,0 +1,14 @@
+class User() {
+    constructor (name, gender, birth, country, email, password, photo, admin) {
+        this.#id;
+        this.#name = name;
+        this.#gender = gender;
+        this.#birth = birth;
+        this.#country = country;
+        this.#email = email;
+        this.#email = email;
+        this.#password = password;
+        this.#photo = photo;
+        this.#admin = admin;
+    }
+}
